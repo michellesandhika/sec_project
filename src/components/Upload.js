76a2ab7718ba9f654262c5981683adc198137712
battheresa <chawisa.batt@gmail.com>
2022-04-02 16:24:41 +0800

    fix upload and modify account settings

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,30 +1,41 @@
 import React, { useState } from "react";
-import { TextField, Button } from "@mui/material";
+
+import { TextField, Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from "@mui/material";
 import AttachFileOutlinedIcon from "@mui/icons-material/AttachFileOutlined";
 import FileUploadOutlinedIcon from "@mui/icons-material/FileUploadOutlined";
+
 import { makeStorageClient } from "../services/ipfs";
 import { useForm } from "react-hook-form";
-
 import "../styles/Upload.css";
-import { Storefront } from "@mui/icons-material";
 
 function Upload() {
+  const iconSize = 100;
   const { register, handleSubmit } = useForm();
+  const [ filename, setFilename ] = useState();
 
-  async function storeFiles(files) {
+  const [ success, setSuccess ] = useState(false);
+  const [ message, setMessage ] = useState('This image has already been uploaded, please upload a new image.');
+  const [ dialog, setDialog ] = useState(false);
+
+  const storeFiles = async (files) => {
     const client = makeStorageClient;
     const cid = await client.put(files);
-    console.log("stored files with cid:", cid);
     return cid;
-  }
-
-  const [filename, setFilename] = useState();
+  };
 
   const onSubmit = (data) => {
     const cid = storeFiles(data.filename);
     console.log(cid);
 
-    // TODO: what next (???)
+    // TODO: upload firesbase firestore (remove comment after done)
+    // if there is duplicate
+    //     -> setMessage('This image has already been uploaded, please upload a new image.');
+    //     -> setSuccess(false);
+    // else 
+    //     -> setMessage('Image uploaded successfully.');
+    //     -> setSuccess(true);
+
+    setDialog(true);
   };
 
   return (
@@ -56,8 +67,9 @@ function Upload() {
               type="file"
               accept="image/*"
               style={{ display: "none" }}
-              onChange={(e) => setFilename(e.target.value)}
-              {...register("filename")}
+              {...register("filename", { 
+                onChange: (e) => setFilename(e.target.value)
+              })}
             />
             <Button
               variant="outlined"
@@ -83,6 +95,16 @@ function Upload() {
           Upload
         </Button>
       </form>
+
+      <Dialog open={dialog} onClose={() => setDialog(false)} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+        <DialogTitle id="alert-dialog-title">{success ? "Success" : "Error"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">{message}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setDialog(false)} variant="outlined">Close</Button>
+        </DialogActions>
+      </Dialog>
     </main>
   );
 }
