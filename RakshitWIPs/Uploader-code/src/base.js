bb68bc857ef919cb97a19ce8bc3a33687c32fa6d
Rakshit Jain <rakshitj60@gmail.com>
2022-04-02 14:26:08 +0800

    Add files via upload

---
+++ RakshitWIPs/Uploader-code/src/base.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+import 'firebase/storage'
+
+export const app = firebase.initializeApp({
+  apiKey: "AIzaSyCYEuzMMZ6FN9x52QRW8Ed4x8BHOJu0f6Q",
+  authDomain: "animated-alloy-239707.firebaseapp.com",
+  databaseURL: "https://animated-alloy-239707-default-rtdb.firebaseio.com",
+  projectId: "animated-alloy-239707",
+  storageBucket: "animated-alloy-239707.appspot.com",
+  messagingSenderId: "455284171296",
+  appId: "1:455284171296:web:df646cbe8e58988fc8d245",
+  measurementId: "G-GFN6NKB0BD"
+});
